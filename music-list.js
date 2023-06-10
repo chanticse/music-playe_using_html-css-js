50d9258c31309e94392c7c135beac61edebe1fc2
chanti y <95081445+chanticse@users.noreply.github.com>
2023-06-10 04:40:28 -0700

    MUSIC

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "jabilli",
+    artist: "Ilayaraja",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Instrumental",
+    artist: "A R rahman",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Dosti",
+    artist: "MM Keeravani",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Jersey",
+    artist: "Anirudh Ravichander",
+    img: "music-6",
+    src: "music-6"
+  }
+];
